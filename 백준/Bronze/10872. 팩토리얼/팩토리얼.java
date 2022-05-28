27385397a83d6b13c834ad14d528b9531d0d5b50
SEong_gu <96984293+SG1515@users.noreply.github.com>
2022-05-29 08:48:33 +0900

    [Bronze III] Title: 팩토리얼, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,32 @@
+//재귀
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int sum = factorial(n);
+		System.out.println(sum);
+		
+
+	}
+	
+	
+	public static int factorial(int n) {
+		if(n <= 1) {
+			return 1;
+		}
+		return n * factorial(n-1);
+		
+		
+	}
+	
+	
+
+}
