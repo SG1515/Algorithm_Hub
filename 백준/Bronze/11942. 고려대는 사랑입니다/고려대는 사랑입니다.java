818f1816ea62f7e22a1b6f201d1d905d0bf7915a
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-01-01 14:54:29 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 116 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,11 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class Main{
+
+	public static void main(String[] args) throws IOException {
+    System.out.println("고려대학교");
+	}
+
+}
