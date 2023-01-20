0a9836232762416b29b7707f5bb5c13da2bd190d
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-01-20 16:39:42 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); 
+		int M = sc.nextInt(); 
+		sc.close();
+
+		if (M == 1 || M == 2) {
+			System.out.println("NEWBIE!");
+		} else if (M <= N) {
+			System.out.println("OLDBIE!");
+		} else {
+			System.out.println("TLE!");
+		}
+	}
+
+}
