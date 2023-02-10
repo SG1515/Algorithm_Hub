da3952630cf32a48fcc82e26b62e9f220ce9337f
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-02-10 22:13:52 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 136 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int k = Math.min(a,b);
+            result = Math.min(k,c);
+            System.out.println(result);
+        }
+    }
+}
