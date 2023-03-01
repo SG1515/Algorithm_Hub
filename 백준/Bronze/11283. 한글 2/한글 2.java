7d0dd36dd151557a1b8764ff9a3492b56becfb6d
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-03-01 23:13:35 +0900

    [Bronze IV] Title: 한글 2, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        System.out.println(br.readLine().charAt(0)-44031);
+    }
+}
