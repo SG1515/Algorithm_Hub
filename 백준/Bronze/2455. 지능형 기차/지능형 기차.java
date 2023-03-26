a47200fcb52fe2fc121e9b6d99773c33e5b1b622
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-03-26 20:12:58 +0900

    [Bronze III] Title: 지능형 기차, Time: 124 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int now = 0;
+        for (int i = 0; i < 4; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int out = Integer.parseInt(st.nextToken());
+            int in = Integer.parseInt(st.nextToken());
+            now -= out;
+            now += in;
+            max = Math.max(max, now);
+        }
+        System.out.print(max);
+    }
+}
