ec379fe46272801bc477fd8379498387fe2b7c39
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-07-07 21:00:59 +0900

    [Bronze II] Title: 이진수 연산, Time: 516 ms, Memory: 28308 KB -BaekjoonHub

---
+++ 백준/Bronze/12813. 이진수 연산/이진수 연산.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[100001];
+        int[] b = new int[100001];
+        String t = sc.nextLine();
+        String tt = sc.nextLine();
+        for(int i=0;i<t.length();i++) {
+            a[i] = t.charAt(i)-'0';
+            b[i] = tt.charAt(i)-'0';
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<t.length();i++)
+            sb.append(a[i]&b[i]);
+        sb.append("\n");
+        for(int i=0;i<t.length();i++)
+            sb.append(a[i]|b[i]);
+        sb.append("\n");
+        for(int i=0;i<t.length();i++)
+            sb.append(a[i]^b[i]);
+        sb.append("\n");
+        for(int i=0;i<t.length();i++)
+            sb.append(a[i]^1);
+        sb.append("\n");
+        for(int i=0;i<t.length();i++)
+            sb.append(b[i]^1);
+        sb.append("\n");
+        System.out.println(sb);
+    }
+}
