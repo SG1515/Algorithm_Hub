809d19280072a47264645f7fddcc6cb479904aa3
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-07-12 23:10:16 +0900

    [Bronze III] Title: 안녕 클레오파트라 세상에서 제일가는 포테이토칩, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/25904. 안녕 클레오파트라 세상에서 제일가는 포테이토칩/안녕 클레오파트라 세상에서 제일가는 포테이토칩.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int idx = 0;
+        while (true) {
+            if (arr[idx] < x) {
+                System.out.println(idx % n + 1);
+                return;
+            }
+            idx++;
+            idx%=n;
+            x++;
+        }
+    }
+}
