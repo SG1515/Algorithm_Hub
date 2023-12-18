06d71c348a52a80534f444d059bf916b07e8ba92
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-12-18 21:54:30 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 116.56 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		/* \와 " 앞에는 백슬래시로 이스케이프 시켜주자.*/
+		System.out.println("!@#$%^&*(\\'\"<>?:;");
+		
+		
+	} //end of main
+
+}
