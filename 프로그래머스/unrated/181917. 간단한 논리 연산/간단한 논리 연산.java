ee9747737645d10eb6fae4a26008639d6d374885
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-12-24 00:35:01 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -2,43 +2,8 @@
 class Solution {
     
     public Boolean solution(Boolean x1, Boolean x2, Boolean x3, Boolean x4) {
-        Boolean answer;
-        // V = 하나라도 T면 T 
-        // ^ = 하나라도 F면 F 
         
-        Boolean tmp1 = true;
-        Boolean tmp2 = true;
-
-        if (x1 == true && x2 == true)
-            tmp1 = true;
-            else if (x1 == true && x2 != true)
-            tmp1 = true;
-            else if (x1 != true && x2 == true)
-            tmp1 = true;
-            else 
-            tmp1 = false;
-
+return (x1||x2)&&(x3||x4);
         
-        if (x3 == true && x4 == true)
-            tmp2 = true;
-            else if (x3 == true && x4 != true)
-            tmp2 = true;
-            else if (x3 != true && x4 == true)
-            tmp2 = true;
-            else 
-            tmp2 = false;
-        Boolean result = true;
-        
-        if (tmp1 == true && tmp2 == true)
-            result = true;
-            else if (tmp1 == true && tmp2 != true)
-            result = false;
-            else if (tmp1 != true && tmp2 == true)
-            result = false;
-            else 
-            result = false;
-    
-        answer = result;
-        return answer;
     }
 }
