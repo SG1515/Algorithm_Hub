b04b68051692e700496d5b588bb90b3e2570f9dd
Seong_gu <96984293+SG1515@users.noreply.github.com>
2023-12-29 00:00:27 +0900

    [level unrated] Title: 배열 만들기 1, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+class Solution {
+    
+    public int[] solution(int n, int k) {
+        int arrSize = n/k;
+        int[] answer = new int[arrSize];
+        
+
+        for(int i=1; i<=n/k; i++){
+            answer[i-1] = k * i ; 
+        }
+        
+        
+        return answer;
+    }
+}
