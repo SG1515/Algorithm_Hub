1e9e064a849238bc220b6d52d826010a16c2e5aa
Seong_gu <96984293+SG1515@users.noreply.github.com>
2024-01-05 21:30:16 +0900

    [level unrated] Title: 1로 만들기, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int cnt =0;
+        int result = 0;
+        for(int i=0; i<num_list.length; i++) {
+            cnt =0;
+            int n = num_list[i];
+            while(n != 0){
+                if(n == 1) break;
+                n /= 2;
+                cnt++;
+            }
+            result += cnt;
+        }
+        
+        return answer = result;
+    }
+}
