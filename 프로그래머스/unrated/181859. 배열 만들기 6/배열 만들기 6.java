a288b906046f7c4c2f536e31b72e3be21c17ad52
Seong_gu <96984293+SG1515@users.noreply.github.com>
2024-01-10 20:53:06 +0900

    [level unrated] Title: 배열 만들기 6, Time: 20.82 ms, Memory: 99.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        int i=0;
+        
+        while (i < arr.length){
+            if(stk.isEmpty()){
+                stk.add(arr[i]);
+                i++;
+            } else if(stk.get(stk.size()-1) == arr[i]){
+                stk.remove(stk.size()-1);
+                i++;
+            } else{
+                stk.add(arr[i]);
+                i++;
+            }
+        }
+        
+        if(stk.isEmpty()){
+            return new int[]{-1};
+        }
+        
+        int[] answer = new int[stk.size()];
+        for(int j=0; j<stk.size(); j++){
+            answer[j] = stk.get(j);
+        }
+        
+        
+        return answer;
+    }
+}
