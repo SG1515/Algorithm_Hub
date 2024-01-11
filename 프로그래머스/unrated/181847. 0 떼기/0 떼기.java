6783222bebcc272edcf9ec8efa80f4074a058a7f
Seong_gu <96984293+SG1515@users.noreply.github.com>
2024-01-11 22:28:56 +0900

    [level unrated] Title: 0 떼기, Time: 9.10 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        int tmp = Integer.parseInt(n_str);
+        answer += "" + tmp;
+        
+        
+        
+        return answer;
+    }
+}
