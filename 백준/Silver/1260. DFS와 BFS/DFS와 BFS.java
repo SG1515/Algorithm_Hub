3b2234d2a22ccbb191ed5def5bedffc3ea7c97fc
Seong_gu <96984293+SG1515@users.noreply.github.com>
2024-02-29 21:17:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 348 ms, Memory: 21260 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,80 +1,79 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Main {
-        static int n;
-        static int[] pn;
+import java.io.*;
+import java.util.*;
+//우왘 지원아 힌트 정답 한개도 안보고 풀었숨ㅋ 굿
+public class Main{
+    static boolean[] visited;
+    static ArrayList<Integer>[] list;
+    static int n, m, v;
+
+    public static void DFS(int v){
+        visited[v] = true;
+        System.out.print(v + " ");
+        
+        for(int next : list[v]){
+            if(!visited[next]){
+                DFS(next);
+            }
+        }
+        
+    }  // end of dfs
+    
+    public static void BFS(int v){
+        Queue<Integer> Q = new LinkedList<>();
+        Q.offer(v);
+        visited[v] = true;
+        
+        while(!Q.isEmpty()){
+            int tmp = Q.poll();
+            System.out.print(tmp + " ");
+            
+            for(int x : list[tmp]){
+                if(!visited[x]){
+                    Q.offer(x);
+                    visited[x] = true;
+                }
+            }
+            
+        }
+        
+    }// end of bfs
+
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken()); // 정점 개수
-        int m = Integer.parseInt(st.nextToken()); // 간선 개수
-        int v = Integer.parseInt(st.nextToken()); // 시작점
-        pn = new int[n];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
         
-        boolean[] visited = new boolean[n+1];
-        ArrayList<Integer>[] list = new ArrayList[n+1];
-        for(int i=0; i<list.length; i++){
+        list = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        
+        for(int i=0; i<=n; i++){
             list[i] = new ArrayList<Integer>();
         }
         
-        //인접리스트 생성
-        // 무방향 X
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+
+            //양방향
             list[a].add(b);
             list[b].add(a);
         }
-
-        // 자식이 여러개면 노드 번호가 작은 것 먼저 방문하므로 오른차순 정렬
-        for(int i=0;i<list.length; i++){
-            Collections.sort(list[i]); //매 배열에 마다 정렬하기
-        }
         
-        DFS(list, visited, v);
+        //정점번호가 작은 것부터 가게 정리하기
+        for(int i=0; i<list.length; i++) {
+        	Collections.sort(list[i]);
+        }
+
+        DFS(v);
         Arrays.fill(visited, false);
         System.out.println();
-        BFS(list, visited, v);
+        BFS(v);
         
-    } //end of main
-
-
-    public static void DFS(ArrayList<Integer>[] list, boolean[] visited, int v){
-        if(visited[v] == true) return; // 방문했으면 빠져나가기
-            
-        visited[v] = true; // 방문 표시 
-        System.out.print(v + " "); //현재 방문된 곳 출력
-        
-        for(int x : list[v]){ // 시작점이랑 연결된 곳 순회하기
-            if(!visited[x]) // 연결된 곳이 true가 아니면 
-            DFS(list, visited, x);
-        }   
-    }
-
-    public static void BFS(ArrayList<Integer>[] list, boolean[] visited, int v){
-        Queue<Integer> Q = new LinkedList<>();
-        Q.offer(v); //시작점 넣기
-        visited[v] = true; // 시작점 방문 표시
-        
-        while(!Q.isEmpty()) {
-            v = Q.poll(); //큐에서 꺼내서 시작점 갱신
-            System.out.print(v+" ");
-
-            for(int x : list[v]){ //시작점이랑 연결된 곳 순회하기
-                if(!visited[x]){ // 연결된 곳이 true가 아니면
-                    Q.offer(x); // 그곳을 큐에 넣기
-                    visited[x]=true; // 연결된 곳 true로 바꾸기
-                }
-            }
-        }
-    }
-
-
-
-    
+    }   
 }
-
