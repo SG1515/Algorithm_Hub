f1c6efc17852050e74ab481a2c44185ef26cd975
Seong_gu <96984293+SG1515@users.noreply.github.com>
2024-03-07 22:14:45 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,27 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main {
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int max = 0;
+
+        int n = Integer.parseInt(st.nextToken()); //card count
+        int m = Integer.parseInt(st.nextToken()); //max number
+        
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){ 
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i+1; j < n - 1; j++) {
-                for (int k = j+1; k < n; k++) {
-                    int sum = arr[i] + arr[j] + arr[k];  
-                    if (sum <= m) { 
-                        max = Math.max(max, sum);
+        
+          
+        int result = 0;
+        for(int k=0; k<n-2; k++){
+            for(int j=k+1; j<n-1; j++){
+                for(int i=j+1; i<n; i++){
+                    
+                    int max = arr[i] + arr[j] + arr[k];
+                    if(m == max) {
+                        result = max;
+                        break;
+                        
+                    }
+                    if (result < max && max < m){
+                        result = max;
                     }
                 }
             }
-        }
-        System.out.print(max);
+        }          
+        
+        System.out.println(result);
     }
 }
