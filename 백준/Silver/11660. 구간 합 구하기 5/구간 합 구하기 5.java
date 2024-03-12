8c7809be1a8d02773fa4257b5f12c9d0dd313ddb
Seong_gu <96984293+SG1515@users.noreply.github.com>
2024-03-12 21:41:37 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1684 ms, Memory: 129320 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,43 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.io.IOException;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); //배열의 크기
-        int M = Integer.parseInt(st.nextToken()); //질의수
-        int A[][] = new int[N+1][N+1];
 
-        for(int i=1; i<=N; i++){ // 원본 배열 저장
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] D = new int[n+1][n+1];
+        int[][] A = new int[n+1][n+1];
+
+        for(int i=1; i<=n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=N; j++){
-             A[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=1; j<=n; j++){
+                D[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        int D[][] = new int[N+1][N+1]; // 합 배열
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
-                D[i][j] = D[i][j-1]+D[i-1][j]-D[i-1][j-1]+A[i][j]; //구간 합
+        //누적합 배열
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++){
+                A[i][j] = A[i][j-1] + A[i-1][j] - A[i-1][j-1] + D[i][j]; 
             }
         }
-
-        for(int i=0; i < M; i++){
+        
+        for (int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-
-            //구간 합 배열로 질의에 답변
-            int result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1];
-            System.out.println(result);
+            int fx = Integer.parseInt(st.nextToken());
+            int fy = Integer.parseInt(st.nextToken());
+            int lx = Integer.parseInt(st.nextToken());
+            int ly = Integer.parseInt(st.nextToken());
+            
+            int answer = A[lx][ly] - A[lx][fy-1] - A[fx-1][ly] + A[fx-1][fy-1];
+            System.out.println(answer);
         }
-        br.close();
-    }
+        
+        
+    
+    }//end of main
 }
+
+
