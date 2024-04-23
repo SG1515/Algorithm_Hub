223973ddefc056626ae3ab572a7719af641cf7f1
Seong_gu <96984293+SG1515@users.noreply.github.com>
2024-04-23 16:01:08 +0900

    [Silver V] Title: 셀프 넘버, Time: 164 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,37 +1,35 @@
-//self number
-public class Main {
+import java.io.*;
+import java.util.*;
 
-	public static void main(String[] args) {
+public class Main {
+	static boolean[] check;
+	public static void main(String[] args) throws IOException{ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		boolean[] check = new boolean[10001]; //1~ 10000까지이므로
+		check = new boolean[10001];
 		
-		
-		for (int i = 1; i < 10001; i++) { // 1~10001까지 검사
-			int n = d(i); // n은 d(n) 생성자
-			
-			if (n < 10001) { //10000넘는 수 X
-				check[n] = true;
-			}	
+		//생성자
+		for(int i=1; i<check.length; i++) {
+			if(d(i) < check.length )
+				check[d(i)] = true;
 		}
 		
-		for (int i = 1; i< 10001; i++) {
-			if (!check[i]) {
-				System.out.print(i + "\n");
+		//출력
+		for(int i=1; i<check.length; i++) {
+			if(!check[i]) {
+				System.out.println(i);
 			}
 		}
 		
+		
 	}
 	
-	
-	
-	public static int d(int number) { //return-> number생성자 d
+	public static int d(int num) {
+		int sum = num;
 		
-		int sum = number; // 연산 전 숫자 저장
-		
-		while(number != 0) {
-			sum = sum + (number % 10); //첫째 자리수
-			number = number/10; // 10을 나누어 한자리씩 줄인다
-			
+		while(num != 0) { // 각 자리 더하기
+			sum += num % 10;
+			num /= 10;
 		}
 		
 		return sum;
