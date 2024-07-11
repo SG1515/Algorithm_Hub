d44350e07afb4c886bd47701ec861e76db3a1764
Seong_gu <96984293+SG1515@users.noreply.github.com>
2024-07-11 17:20:12 +0900

    [Bronze IV] Title: 숫자의 합, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,26 +1,20 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+ 
 public class Main {
+   public static void main(String[] args) throws IOException{      
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt(); //숫자의 갯수
-		String a = sc.next(); //숫자 N개가 공백없이 주어짐 이때, 입력된 숫자는 문자열로 저장
-		sc.close();
-		
-		
-		int sum = 0;
+       int n = Integer.parseInt(br.readLine());
+        String number = br.readLine();
 
-		
-		for(int i = 0; i < n; i++) {
-			
-			sum = sum + a.charAt(i)-'0';
-			//입력 받은 문자열의 첫번째 원소 charAt(0) ~ charAt(n)까지 원소의 누적 합 구하기
-			//이때 charAt()는 문자의 아스키코드 값을 반환하므로 -48 또는 '0'을 해줘야 함
-		}
-		
-		System.out.println(sum);
-	}
+       String[] str = number.split("");
+        int sum=0;
+       for(int i=0; i<str.length; i++) {
+           sum += Integer.parseInt(str[i]);
+       }
+       
 
+       System.out.print(sum);
+   }
 }
