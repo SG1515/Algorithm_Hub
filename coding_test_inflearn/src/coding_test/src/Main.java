ea5284be86990394bf8e181b339f27c6dbde39ea
SG1515 <sunggu507@naver.com>
2026-09-15 14:09:36 +0900

    [인프런] java 9_6

--- coding_test_inflearn/src/coding_test/src/Main.java
+++ coding_test_inflearn/src/coding_test/src/Main.java
@@ -6,34 +6,22 @@ public class Main {
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
+		
 		String str = sc.next();
 		System.out.println(T.solution(str));
-		
-//		char a = "A".charAt(0);
-//		char b = "z".charAt(0);
-//		System.out.println((int)a + " " + (int)b);
 	}
-	
 	
 	public String solution(String str) {
 		String result = "";
 		
-		char[] strArr = str.toCharArray();
-		int lt = 0; 
-		int rt = str.length()-1;
-
-		while(lt<rt) {
-			if(!Character.isAlphabetic(strArr[lt])) lt++;
-			else if(!Character.isAlphabetic(strArr[rt])) rt--;
-			else {
-				char tmp = strArr[lt];
-				strArr[lt] = strArr[rt];
-				strArr[rt] = tmp;
-				lt++;
-				rt--;
-			}
+		
+		for(int i=0; i<str.length(); i++) {
+			/** 
+			 * indexOf 와 i의 값이 같으면 처음 등장한 문자
+			 */
+			//System.out.println(str.charAt(i) + " " + i + " " +  str.indexOf(str.charAt(i)));
+			if(str.indexOf(str.charAt(i)) == i)  result += str.charAt(i);
 		}
-		result = String.valueOf(strArr);
 		
 		return result;
 	}
