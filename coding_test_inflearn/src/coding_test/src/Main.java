5a7752c2a8b152958b34617bc2ba396ad0fcad64
SG1515 <sunggu507@naver.com>
2026-09-15 17:07:30 +0900

    [인프런] java 12_10 문자거리

--- coding_test_inflearn/src/coding_test/src/Main.java
+++ coding_test_inflearn/src/coding_test/src/Main.java
@@ -1,38 +1,53 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
    
 	public static void main(String[] args) throws IOException{
 		Main T = new Main();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String str = br.readLine();
-		System.out.println(T.solution(str));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String str = st.nextToken();
+		char s = st.nextToken().charAt(0);
+		System.out.println(T.solution(str, s));
 	}
 	
-	public String solution(String str) {
-		String result = "YES";
-		StringBuilder sb = new StringBuilder();
+	public String solution(String str, char s) {
+		String result = "";
+	
+		int strLength = str.length();
+		int[] distance = new int[strLength];
+		int count = 1000;
+
 		
-		str = str.toUpperCase();
-		char[] strArr = str.toCharArray();
-		for (char x : strArr) {
-			if(x >= 65 && x <= 122) {
-				sb.append(x);
+		for (int i=0; i<str.length(); i++) {
+			if(s == str.charAt(i)) {
+				count = 0;
+			} else {
+				count++;
 			}
+			distance[i] = count; 
 		}
 		
-		String s = sb.toString();
-		String reverseS = sb.reverse().toString();
-//		System.out.println("s : " + s);
-//		System.out.println("r : " + reverseS);
-		if(!s.equals(reverseS)) {
-			return result = "NO";
+		count = 1000;
+		for(int i=str.length()-1; i>=0; i--) {
+			if(s == str.charAt(i)) {
+				count = 0;
+			} else {
+				count ++;
+			}
+			
+			distance[i] = Math.min(distance[i], count);
+			
 		}
-		
-		
+
+		StringBuilder sb = new StringBuilder();
+		for(int x : distance) {
+			sb.append(x).append(' ');
+		}
+		result = sb.toString().trim();
 		return result;
 	}
 
